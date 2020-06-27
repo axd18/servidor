6d84c125e20003d08599330a0bdad62c42606dfc
adrian <adrianfernandezj@gmail.com>
2020-06-27 20:06:25 -0300

    241-Creando el Controller de Auntenticación

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,18 @@
-// Rutas para crear usuarios
+// Rutas para autenticar usuarios
 const express = require('express');
 const router = express.Router();
-const usuarioController = require('../controllers/usuarioController');
 const { check } = require('express-validator');
+const authController = require('../controllers/authController');
 
 // Crea un usuario
 // se enviará a api/usuarios
 router.post('/',
     [
-        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
         check('email', 'Agrega un email válido').isEmail(),
         check('password', 'El password debe ser mínimo de 6 caracteres'). isLength({min: 6})
     ],
-    usuarioController.crearUsuario
+    authController.autenticarUsuario
+    
 );
 
 module.exports = router;
