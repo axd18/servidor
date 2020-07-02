9e93355006501149add188661bffd812940a3c95
adrian <adrianfernandezj@gmail.com>
2020-07-02 17:29:00 -0300

    245-Insertando nuevos proyectos

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -1,3 +1,15 @@
-exports.crearProyecto = async (req, res) => {
+const Proyecto = require ('../models/Proyecto');
 
+
+exports.crearProyecto = async (req, res) => {
+    try {
+        // Crear un nuevo proyecto
+        const proyecto = new Proyecto(req.body);
+        proyecto.save();
+        res.json(proyecto);
+    
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('hubo un error');
+    }
 }
