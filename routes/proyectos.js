801e44e1f7edf82c06aa1583d5acd97e4d96c5d5
adrian <adrianfernandezj@gmail.com>
2020-07-09 15:33:02 -0300

    246-Verificando si el usuario esta autenticado para crear nuevos proyectos

--- routes/proyectos.js
+++ routes/proyectos.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const proyectoController = require('../controllers/proyectoController');
+const auth = require('../middleware/auth');
 
 // Crea proyectos
 // api/proyectos
 router.post('/',
-    proyectoController.crearProyecto
+auth,
+proyectoController.crearProyecto
+);
+
+router.get('/',
+auth,
+proyectoController.crearProyecto
 )
 
 module.exports = router;
