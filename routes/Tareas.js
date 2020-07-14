9343b189cc9e403f4963a0760fa2c8a49567ca9f
adrian <adrianfernandezj@gmail.com>
2020-07-14 18:16:35 -0300

    Hasta 255-Obtener tareas por proyecto

--- routes/Tareas.js
+++ routes/Tareas.js
@@ -9,9 +9,16 @@ const { check } = require('express-validator');
 router.post('/',
     auth,
     [
-        check('nombre', 'El nombre de la tarea es obligatorio').not().isEmpty()
+        check('nombre', 'El nombre de la tarea es obligatorio').not().isEmpty(),
+        check('nombre', 'El nombre del proyecto es obligatorio').not().isEmpty()
     ],
     tareaController.crearTarea
 );
+
+// Obtener las tareas pro proyecto
+router.get('/',
+    auth,
+    tareaController.obtenerTareas
+)
 
 module.exports = router;
